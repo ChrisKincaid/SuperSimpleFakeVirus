8b8862d69ac8ab1b4404ea283c642d155907097d
ChrisKincaid <christophergkincaid@msn.com>
2023-08-27 23:47:47 -0500

    Add project files.

---
+++ FakeVirus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MessageDisplayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This is not a virus, but it could have been!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
